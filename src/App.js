f69c8e221dba72f6422e2f61c3abaa3650988ead
Akhtar1997 <92727977+Akhtar1997@users.noreply.github.com>
2024-04-09 11:17:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React, { Component } from "react";
-// import Auth from "@aws-amplify/auth";
-// import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
-import awsconfig from "./aws-exports";
-
-
-import "./App.css";
+import { Amplify }  from '@aws-amplify/core';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsconfig from './aws-exports';
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import 'semantic-ui-less/semantic.less';
+import Ccp from './components/ccp';
+//import { autoSignIn } from '@aws-amplify/auth';
 
-// Import components
-import Ccp from "./components/ccp";
+// Component
+function App({ signOut, user }) {
+  const [isConfigured, setIsConfigured] = useState(false);
+  
+  useEffect(() => {
+    configureAuth();
+    //signedIn();
+  }, []);
 
+  const configureAuth = () => {
+    Amplify.configure(awsconfig);
+    setIsConfigured(true);
+  };
+  //const signedIn = async () => {
+    //await autoSignIn();
+  //};
 
-
-class App extends Component {
-       render() {
-              return (
-                     <div className="App">
-                        <Ccp />
-                     </div>
-              );
-       }
+  return (
+    <div className="App">
+      {isConfigured && <Ccp user={user} signOut={signOut} />}
+    </div>
+  );
 }
 
-export default App
+export default withAuthenticator(App);
