d0f51a1531cb330290faa9fe632ba2791668a6f3
Akhtar1997 <92727977+Akhtar1997@users.noreply.github.com>
2024-04-22 11:20:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,24 +6,25 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'semantic-ui-less/semantic.less';
 import Ccp from './components/ccp';
-//import { autoSignIn } from '@aws-amplify/auth';
+import { autoSignIn } from '@aws-amplify/auth';
 
 // Component
 function App({ signOut, user }) {
   const [isConfigured, setIsConfigured] = useState(false);
   
   useEffect(() => {
-    configureAuth();
-    //signedIn();
+    //configureAuth();
+    signedIn();
   }, []);
 
   const configureAuth = () => {
-    Amplify.configure(awsconfig);
+   // Amplify.configure(awsconfig);
     setIsConfigured(true);
   };
-  //const signedIn = async () => {
-    //await autoSignIn();
-  //};
+  const signedIn = async () => {
+    await autoSignIn();
+     setIsConfigured(true);
+  };
 
   return (
     <div className="App">
